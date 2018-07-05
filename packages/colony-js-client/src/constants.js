cb3ef025bb9daa6b62605a98333e8973cf29cb7d
James Lefrère <james@colony.io>
2018-07-05 20:12:02 +0200

    Define `PARAMS`
    * Define commonly-used param types as constants

--- packages/colony-js-client/src/constants.js
+++ packages/colony-js-client/src/constants.js
@@ -21,3 +21,19 @@ export const AUTHORITY_ROLES = {
 };
 
 export const EMPTY_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+export const PARAMS = {
+  ADDRESS: ['address', 'address'],
+  AMOUNT: ['amount', 'bigNumber'],
+  COUNT: ['count', 'number'],
+  DELIVERABLE_HASH: ['deliverableHash', 'ipfsHash'],
+  DOMAIN_ID: ['domainId', 'number'],
+  DUE_DATE: ['dueDate', 'date'],
+  ID: ['id', 'number'],
+  ROLE: ['role', 'role'],
+  SKILL_ID: ['skillId', 'number'],
+  SPEC_HASH: ['specificationHash', 'ipfsHash'],
+  TASK_ID: ['taskId', 'number'],
+  TOKEN: ['token', 'tokenAddress'],
+  USER: ['user', 'address'],
+};
