be6c2223dae530483c0dd2be34264879e2dfe62a
Ryan Christoffersen <ryan@ryanchristo.com>
2018-11-22 16:24:28 +0700

    Fix output conversion for GetTask caller

--- packages/colony-js-client/src/ColonyClient/callers/GetTask.js
+++ packages/colony-js-client/src/ColonyClient/callers/GetTask.js
@@ -5,6 +5,7 @@ import assert from 'assert';
 import BigNumber from 'bn.js';
 import ContractClient from '@colony/colony-js-contract-client';
 import { isBigNumber } from '@colony/colony-js-utils';
+import { TASK_STATUSES } from '../../constants';
 
 import type ColonyClient from '../index';
 
@@ -57,16 +58,41 @@ export default class GetTask extends ContractClient.Caller<
   }
   // eslint-disable-next-line class-methods-use-this
   convertOutputValues(result: CallResult, { taskId }: *) {
-    const task = super.convertOutputValues(result);
+    const values = [].concat(result);
+
+    const parsedResult = this.output.reduce(
+      (acc, [name], index) => Object.assign(acc, { [name]: values[index] }),
+      {},
+    );
+
+    const task = super.convertOutputValues(parsedResult);
 
     // Until arrays of bignumbers are supported as a parameter type,
     // take the last item of the call result (skillIds) and use the first one
     const skillId: BigNumber = [].concat(result[result.length - 1])[0];
 
+    Object.keys(parsedResult).forEach(key => {
+      if (key === 'skillId') {
+        // Convert bignumbers to numbers for skillId
+        parsedResult[key] = isBigNumber(skillId) ? skillId.toNumber() : skillId;
+      } else {
+        // Convert bignumbers to numbers
+        parsedResult[key] = isBigNumber(parsedResult[key])
+          ? parsedResult[key].toNumber()
+          : parsedResult[key];
+      }
+      // Convert status number to name
+      if (key === 'status') {
+        parsedResult[key] = Object.keys(TASK_STATUSES).find(
+          name => TASK_STATUSES[name] === parsedResult[key],
+        );
+      }
+    });
+
     return Object.assign({}, task, {
       // Include the task ID
       id: taskId,
-      skillId: isBigNumber(skillId) ? skillId.toNumber() : null,
+      ...parsedResult,
     });
   }
 }
